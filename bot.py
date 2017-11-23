20481d841aa2ace73f2e3da709a169e92864fe0d
dmcallejo <dmunoz@gipym.com>
2017-11-23 09:40:35 +0100

    Logger. Verbosity selector by argparse

--- bot.py
+++ bot.py
@@ -3,26 +3,43 @@
 import os
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-import logger
-import logging
-import telebot
-from telebot.util import async
+#import telebot
+#from telebot.util import async
 from datetime import datetime
 import time
 import subprocess
 import re
+import argparse
+import logging
+import logger
 
 LOG = logging.getLogger('ASFBot')
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-v", "--verbosity", help="Defines log verbosity", choices=['CRITICAL', 'ERROR', 'WARN', 'INFO', 'DEBUG'], default='INFO')
+
+args = parser.parse_args()
+
+numeric_level = getattr(logging, args.verbosity.upper(), None)
+if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: %s' % args.verbosity )
+for logger in LOG.handlers:
+    logger.setLevel(args.verbosity)
+
 LOG.info("Starting up bot...")
 
-TELEGRAM_API_TOKEN=os.environ.get('TELEGRAM_API_TOKEN') #TODO: ENV or ARGPARSE 
+exit()
+
+TELEGRAM_API_TOKEN=os.environ.get('TELEGRAM_API_TOKEN') #TODO: ENV or ARGPARSE
 if not TELEGRAM_API_TOKEN:
-	LOG.error("Missing TELEGRAM_API_TOKEN env variable")
-	exit(1)
+    LOG.error("Missing TELEGRAM_API_TOKEN env variable")
+    exit(1)
 TELEGRAM_USER_ID=os.environ.get('TELEGRAM_USER_ID')
 if not TELEGRAM_USER_ID:
-	LOG.error("Missing TELEGRAM_USER_ID env variable")
-	exit(1)
+    LOG.error("Missing TELEGRAM_USER_ID env variable")
+    exit(1)
+
 
 ASF_IPC_HOST=os.environ.get('ASF_IPC_HOST') or '127.0.0.1'
 ASF_IPC_PORT=os.environ.get('ASF_IPC_PORT') or '1242'
@@ -43,58 +60,58 @@ bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
 
 @bot.message_handler
 def handler(message):
-	cid = m.chat.id
-	if cid == TELEGRAM_USER_ID:
-		if m.text[0] == '/' or "!":
-			asf_command=m.text[1:]
-			response = execute_command(m,asf_command)
-			bot.reply_to(m,"```"+str(response)+"```",parse_mode="Markdown")
-		else:
-			cdkeys = cdkey_pattern.findall(m.text)
-			if len(cdkeys)>0:
-				bot.reply_to(m,"Found: "+str(len(cdkeys))+" cdkeys.")
-				#auto redeem
-				for cdkey in cdkeys:
-					command = "redeem "+asf_primary_bot+" "+cdkey
-					response = execute_command(m,command)
-					bot.send_message(cid,"```"+str(response)+"```",parse_mode="Markdown")
+    cid = m.chat.id
+    if cid == TELEGRAM_USER_ID:
+        if m.text[0] == '/' or "!":
+            asf_command=m.text[1:]
+            response = execute_command(m,asf_command)
+            bot.reply_to(m,"```"+str(response)+"```",parse_mode="Markdown")
+        else:
+            cdkeys = cdkey_pattern.findall(m.text)
+            if len(cdkeys)>0:
+                bot.reply_to(m,"Found: "+str(len(cdkeys))+" cdkeys.")
+                #auto redeem
+                for cdkey in cdkeys:
+                    command = "redeem "+asf_primary_bot+" "+cdkey
+                    response = execute_command(m,command)
+                    bot.send_message(cid,"```"+str(response)+"```",parse_mode="Markdown")
 
-			else:
-				log_line=m.text
-	else:
-		log_line="Not authorised: "+m.text
-		log(m,log_line)
-		bot.send_message(cid,"https://www.youtube.com/watch?v=glojDYsGAvo")
+            else:
+                log_line=m.text
+    else:
+        log_line="Not authorised: "+m.text
+        log(m,log_line)
+        bot.send_message(cid,"https://www.youtube.com/watch?v=glojDYsGAvo")
 
 
 bot.set_update_listener(listener)
 
 def execute_command(m,command):
-	os_command=asf_prepend+command+asf_append
-	log(m,"Executing: "+os_command)
-	stdout=str(subprocess.check_output(os_command.split(' ',3)))
-	slices=stdout.split('WCF response received:',1)
-	if(len(slices)>1):
-		response=slices[1]
-	else:
-		response=stdout
-	return str(response).strip()
+    os_command=asf_prepend+command+asf_append
+    log(m,"Executing: "+os_command)
+    stdout=str(subprocess.check_output(os_command.split(' ',3)))
+    slices=stdout.split('WCF response received:',1)
+    if(len(slices)>1):
+        response=slices[1]
+    else:
+        response=stdout
+    return str(response).strip()
 
 def log(m,text):
-	cid = m.chat.id
-	now = datetime.now().strftime("%Y-%m-%d %H:%M")
-	username = get_chat_username(m)
-	log_message = ("[%s][%s][%s][%s]" % (now, cid, username, text))
-	f = open(log_path + log_file, 'a')
-	f.write(log_message + "\n")
-	f.close()
+    cid = m.chat.id
+    now = datetime.now().strftime("%Y-%m-%d %H:%M")
+    username = get_chat_username(m)
+    log_message = ("[%s][%s][%s][%s]" % (now, cid, username, text))
+    f = open(log_path + log_file, 'a')
+    f.write(log_message + "\n")
+    f.close()
 
 def get_chat_username(m):
-	cid = m.chat.id
-	if cid > 0:
-		return m.chat.first_name
-	else:
-		return m.from_user.first_name
+    cid = m.chat.id
+    if cid > 0:
+        return m.chat.first_name
+    else:
+        return m.from_user.first_name
 
 
 #############################################
@@ -103,8 +120,8 @@ def get_chat_username(m):
 # Con esto, le decimos al bot que siga funcionando
 # incluso si encuentra algún fallo.
 while True:
-	try:
-		bot.polling(none_stop=True)
-	except Exception as e:
-		_error=True
-		bot.send_message(_boss,e)
+    try:
+        bot.polling(none_stop=True)
+    except Exception as e:
+        _error=True
+        bot.send_message(_boss,e)
--- logger.py
+++ logger.py
@@ -1,14 +1,15 @@
 import logging
+DEFAULT_LOG_LEVEL=logging.INFO
 
 # create logger with 'spam_application'
 logger = logging.getLogger('ASFBot')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(DEFAULT_LOG_LEVEL)
 # create file handler which logs even debug messages
 fh = logging.FileHandler('ASFBot.log')
-fh.setLevel(logging.DEBUG)
+fh.setLevel(DEFAULT_LOG_LEVEL)
 # create console handler with a higher log level
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+ch.setLevel(DEFAULT_LOG_LEVEL)
 # create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
